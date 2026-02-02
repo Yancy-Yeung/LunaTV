0f6b06559af5faaa0f5b82c4034bb939d24a1c60
Your Name <your.email@example.com>
2026-02-02 23:01:05 +0800

    feat: update for token expiration

--- src/app/api/login/route.ts
+++ src/app/api/login/route.ts
@@ -110,7 +110,7 @@ export async function POST(req: NextRequest) {
         true,
       ); // localstorage 模式包含 password
       const expires = new Date();
-      expires.setDate(expires.getDate() + 30); // 30天过期
+      expires.setDate(expires.getDate() + 365); // 365天过期
 
       response.cookies.set('auth', cookieValue, {
         path: '/',
